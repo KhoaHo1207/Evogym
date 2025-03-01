e4ac13f7e40815ae7810c76bf035c89b543507ac
Khoa <khoaho120703@gmail.com>
2025-03-01 22:49:05 +0700

    Fix before deploying

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -2,7 +2,7 @@ import { SelectedPage } from "@/shared/types";
 import ActionButton from "@/shared/ActionButton";
 import HomePageText from "@/assets/images/HomePageText.png";
 import HomePageGraphic from "@/assets/images/HomePageGraphic.png";
-import SponsorRedBull from "@/assets/images/SponsorRedBull.png";
+// import SponsorRedBull from "@/assets/images/SponsorRedBull.png";
 import SponsorForbes from "@/assets/images/SponsorForbes.png";
 import SponsorFortune from "@/assets/images/SponsorFortune.png";
 import useMediaQuery from "@/hooks/useMediaQuery";
@@ -80,7 +80,7 @@ function Home({ setSelectedPage }: Props) {
         <div className="h-[150px] w-full bg-primary-100 py-10">
           <div className="mx-auto w-5/6">
             <div className="flex w-3/5 items-center justify-between gap-8">
-              <img src={SponsorRedBull} alt="rebbull-sponsor" />
+              {/* <img src={SponsorRedBull} alt="rebbull-sponsor" /> */}
               <img src={SponsorForbes} alt="forbes-sponsor" />
               <img src={SponsorFortune} alt="fortune-sponsor" />
             </div>
